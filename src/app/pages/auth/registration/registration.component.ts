e78852d3dc7aa9157c8619b846c96e162a57bb18
6iller <a.sabitov.msu.moskau@gmail.com>
2025-03-10 17:54:28 +0300

    acc to lections

--- src/app/pages/auth/registration/registration.component.ts
+++ src/app/pages/auth/registration/registration.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-registration',
-  imports: [],
+  imports: [ButtonModule, CheckboxModule, InputTextModule, FormsModule, NgClass],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
-export class RegistrationComponent { }
+export class RegistrationComponent implements OnInit { 
+  login: string;
+  password: string;
+  repeatPassword: string;
+  cardNumber: string;
+  email: string;
+  isRemember: boolean;
+  labelText = 'Сохранить пользователя в хранилище';
+  constructor () {
+  }
+    ngOnInit(): void {
+
+    }
+    onAuth(): void {
+  }
+ 
+}
